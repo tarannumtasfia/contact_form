39b20e2ba61eaa99785ba9570a0d068434ba3e3e
tarannumtasfia <tasfiatarannum822.com>
2025-06-30 13:24:05 +0600

    got some update

--- server.js
+++ server.js
@@ -38,16 +38,12 @@ console.log("🔍 Request params:", params.toString());
 
 
 
-// Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE,
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT),
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // Handle form submission
